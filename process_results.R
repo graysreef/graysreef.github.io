ecdae123b0127e6ea278fb5ea73e772c1cf45402
Stephanie Gad <sgad@esm.ucsb.edu>
2017-04-25 14:06:20 -0700

    cleaned up script

--- process_results.R
+++ process_results.R
@@ -82,28 +82,30 @@ process_sppyr_dirs = function(dir_sppyr, ...){
   }
 }
 
+
 summarize_sppyr = function(dir_sppyr){
 
   dirs_results = list.files(dir_sppyr, '.*_results$', full.names=T)
   rasters_quantity = sprintf('%s/quantity.tif', dirs_results)
   stack_quantity = stack(rasters_quantity) #gathers all the rasters_quantity together
-  
+
   r_mean = mean(stack_quantity, na.rm=T)
   r_sd = calc(stack_quantity, fun=function(x) sd(x, na.rm=T))
   r_cv = r_sd / r_mean * 100
-  
-  for (v in c('mean','cv')){
-    
+  r_sum = sum(stack_quantity, na.rm=T) # %>% mask(r_mask)
+
+  for (v in c('mean','cv','sum')){
+
     r = get(sprintf('r_%s',v))
-    
+
     # write to GeoTIFF
     writeRaster(r, sprintf('%s/%s.tif', dir_sppyr, v), overwrite=T)
-    
+
     # plot to PNG for easy preview
     png(sprintf('%s/%s.png', dir_sppyr, v))
     p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_sppyr), v))
     print(p)
-    dev.off()  
+    dev.off()
 
   }
 }
@@ -114,71 +116,143 @@ summarize_spp = function(dir_root, sp){
   # summarize sp_yr/mean.tif across years as sp/mean.tif and sp/cv.tif,
   # ie average dispersal across year means and variation across year means
   # dir_root = 'G:/Team_Folders/Steph'; sp='bsb'
-  
+
+  # dirs_results = list.files(dir_root, sprintf('%s_[0-9]{4}$', sp), full.names=T)
   dirs_results = list.files(dir_root, sprintf('%s_[0-9]{4}$', sp), full.names=T)
   rasters_mean = sprintf('%s/mean.tif', dirs_results)
   stack_mean   = stack(rasters_mean)
   dir_sp = file.path(dir_root, sp)
-  
+
   if (!file.exists(dir_sp)) dir.create(dir_sp) ##creates the folder with species name (if it doesn't already exist)
-  
+
   r_mean = mean(stack_mean, na.rm=T)
   r_sd = calc(stack_mean, fun=function(x) sd(x, na.rm=T))
   r_cv = r_sd / r_mean * 100
-  
-  for (v in c('mean','cv')){
-    
+
+  for (v in c('mean','cv','sd')){
+
     r = get(sprintf('r_%s',v))
-    
+
     # write to GeoTIFF
     writeRaster(r, sprintf('%s/%s.tif', dir_sp, v), overwrite=T)
-    
+
     # plot to PNG for easy preview
     png(sprintf('%s/%s.png', dir_sp, v))
     p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_sp), v))
     print(p)
-    dev.off()  
-    
+    dev.off()
+
   }
 }
+
+# ##for the sum instead of mean
+# summarize_spp = function(dir_root, sp){
+#   # given top-level directory and species code, eg "sp" or "rs" or "bsb",
+#   # summarize sp_yr/mean.tif across years as sp/mean.tif and sp/cv.tif,
+#   # ie average dispersal across year means and variation across year means
+#   # dir_root = 'G:/Team_Folders/Steph'; sp='bsb'
+#   
+#   dirs_results = list.files(dir_root, sprintf('%s_[0-9]{4}$', sp), full.names=T)
+#   rasters_sum = sprintf('%s/sum.tif', dirs_results)
+#   stack_sum   = stack(rasters_sum)
+#   dir_sp = file.path(dir_root, sp)
+#   
+#   if (!file.exists(dir_sp)) dir.create(dir_sp) ##creates the folder with species name (if it doesn't already exist)
+#   
+#   r_mean = mean(stack_sum, na.rm=T)
+#   r_sd = calc(stack_sum, fun=function(x) sd(x, na.rm=T))
+#   r_cv = r_sd / r_mean * 100
+#   
+#   for (v in c('mean','cv','sd')){
+#     
+#     r = get(sprintf('r_%s',v))
+#     
+#     # write to GeoTIFF
+#     writeRaster(r, sprintf('%s/%s_sum.tif', dir_sp, v), overwrite=T)
+#     
+#     # plot to PNG for easy preview
+#     png(sprintf('%s/%s_sum.png', dir_sp, v))
+#     p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_sp), v))
+#     print(p)
+#     dev.off()  
+#     
+#   }
+# }
+
 
 #summarize_spp('G:/Team_Folders/Steph', sp='bsb')
 for (sp in c('bsb','gg','rs','sp')){
   summarize_spp('G:/Team_Folders/Steph', sp)  
 }
 
+
+
 summarize_spp = function(dir_root='G:/Team_Folders/Steph', spp=c('bsb','gg','rs','sp')){
-  # given top-level directory and species code, eg "sp" or "rs" or "bsb",
-  # summarize sp_yr/mean.tif across years as sp/mean.tif and sp/cv.tif,
-  # ie average dispersal across year means and variation across year means
-  # dir_root = 'G:/Team_Folders/Steph'; sp='bsb'
-  
-  dirs_results = file.path(dir_root, spp)
-  rasters_mean = sprintf('%s/mean.tif', dirs_results)
-  stack_mean   = stack(rasters_mean)
-  dir_spp = file.path(dir_root, '_allspp')
-  
-  if (!file.exists(dir_spp)) dir.create(dir_spp)
-  
-  r_mean = mean(stack_mean, na.rm=T)
-  r_sd = calc(stack_mean, fun=function(x) sd(x, na.rm=T))
-  r_cv = r_sd / r_mean * 100
-  
-  for (v in c('mean','cv')){
-    
-    r = get(sprintf('r_%s',v))
-    
-    # write to GeoTIFF
-    writeRaster(r, sprintf('%s/%s.tif', dir_spp, v), overwrite=T)
-    
-    # plot to PNG for easy preview
-    png(sprintf('%s/%s.png', dir_spp, v))
-    p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_spp), v))
-    print(p)
-    dev.off()  
-    
-  }
+   # given top-level directory and species code, eg "sp" or "rs" or "bsb",
+   # summarize sp_yr/mean.tif across years as sp/mean.tif and sp/cv.tif,
+   # ie average dispersal across year means and variation across year means
+   # dir_root = 'G:/Team_Folders/Steph'; sp='bsb'
+
+   dirs_results = file.path(dir_root, spp)
+   rasters_mean = sprintf('%s/mean.tif', dirs_results)
+   stack_mean   = stack(rasters_mean)
+   dir_spp = file.path(dir_root, '_allspp')
+
+   if (!file.exists(dir_spp)) dir.create(dir_spp)
+
+   r_mean = mean(stack_mean, na.rm=T)
+   r_sd = calc(stack_mean, fun=function(x) sd(x, na.rm=T))
+   r_cv = r_sd / r_mean * 100
+
+   for (v in c('mean','cv')){
+
+     r = get(sprintf('r_%s',v))
+
+     # write to GeoTIFF
+     writeRaster(r, sprintf('%s/%s.tif', dir_spp, v), overwrite=T)
+
+     # plot to PNG for easy preview
+     png(sprintf('%s/%s.png', dir_spp, v))
+     p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_spp), v))
+     print(p)
+     dev.off()
+
+   }
 }
+
+# # ##for the sum instead of mean
+# summarize_spp = function(dir_root='G:/Team_Folders/Steph', spp=c('bsb','gg','rs','sp')){
+#   # given top-level directory and species code, eg "sp" or "rs" or "bsb",
+#   # summarize sp_yr/mean.tif across years as sp/mean.tif and sp/cv.tif,
+#   # ie average dispersal across year means and variation across year means
+#   # dir_root = 'G:/Team_Folders/Steph'; sp='bsb'
+#   
+#   dirs_results = file.path(dir_root, spp)
+#   rasters_mean = sprintf('%s/mean_sum.tif', dirs_results)
+#   stack_mean   = stack(rasters_mean)
+#   dir_spp = file.path(dir_root, '_allspp')
+#   
+#   if (!file.exists(dir_spp)) dir.create(dir_spp)
+#   
+#   r_mean = mean(stack_mean, na.rm=T)
+#   r_sd = calc(stack_mean, fun=function(x) sd(x, na.rm=T))
+#   r_cv = r_sd / r_mean * 100
+#   
+#   for (v in c('mean','cv')){
+#     
+#     r = get(sprintf('r_%s',v))
+#     
+#     # write to GeoTIFF
+#     writeRaster(r, sprintf('%s/%s_sum.tif', dir_spp, v), overwrite=T)
+#     
+#     # plot to PNG for easy preview
+#     png(sprintf('%s/%s_sum.png', dir_spp, v))
+#     p = levelplot(r, par.settings=viridisTheme, main=sprintf('%s %s', basename(dir_spp), v))
+#     print(p)
+#     dev.off()  
+#     
+#   }
+# }
 
 summarize_spp(dir_root='G:/Team_Folders/Steph', spp=c('bsb','gg','rs','sp'))
 
@@ -191,12 +265,12 @@ library(tidyverse)
 library(raster)
 library(plotly)
 
-r = raster('G:/Team_Folders/Steph/_allspp/mean.tif')
+r = raster('G:/Team_Folders/Steph/_allspp/percent_sum_noseagrass.tif')
 
 d = data_frame(
   quantity = raster::getValues(r),
   cellid   = 1:length(quantity),
-  area_km2 = 8)
+  area_km2 = 64)
 
 d2 = d %>%
   filter(!is.na(quantity)) %>%
@@ -250,31 +324,53 @@ leaflet() %>%
     title = "cum % larvae")
 
 
-d_30 = d2 %>% filter(cum_pct_quantity >= 30) %>% head(1)
+d_20 = d2 %>% filter(cum_pct_quantity >= 20) %>% head(1)
 
-d_10 = d2 %>% filter(cum_pct_quantity >=10) %>% head(1)
+d_40 = d2 %>% filter(cum_pct_quantity >=40) %>% head(1)
 
-d_50 = d2 %>% filter(cum_pct_quantity >= 50) %>% head(1)
+d_60 = d2 %>% filter(cum_pct_quantity >= 60) %>% head(1)
+
+max_area_km2 = d2 %>%
+  arrange(cum_pct_quantity) %>%
+  filter(cum_pct_quantity == 100) %>%
+  head(1) %>%
+  .$cum_area_km2
+
+formatter1000 <- function(x){ 
+  x/1000 
+}
 
 plot(r)
 p = ggplot(d2, aes(y=cum_pct_quantity, x=cum_area_km2)) +
-  xlab("Cumulative Area km2") +
-  ylab("Cumulative Percent Quantity Larvae") +
-  ggtitle("All Species 2009 - 2015") +
-  geom_point() +
-  geom_segment(x=0, xend=d_30$cum_area_km2, y=d_30$cum_pct_quantity, yend=d_30$cum_pct_quantity) +
-  geom_segment(x=d_30$cum_area_km2, xend=d_30$cum_area_km2, y=0, yend=d_30$cum_pct_quantity) +
-  geom_segment(x=0, xend=d_10$cum_area_km2, y=d_10$cum_pct_quantity, yend=d_10$cum_pct_quantity) +
-  geom_segment(x=d_10$cum_area_km2, xend=d_10$cum_area_km2, y=0, yend=d_10$cum_pct_quantity) +
-  geom_segment(x=0, xend=d_50$cum_area_km2, y=d_50$cum_pct_quantity, yend=d_50$cum_pct_quantity) +
-  geom_segment(x=d_50$cum_area_km2, xend=d_50$cum_area_km2, y=0, yend=d_50$cum_pct_quantity) +
-  scale_y_continuous(expand = c(0,0), breaks = c(10,20,30,40,50,60,70,80,90,100)) + scale_x_continuous(expand = c(0,0))
+  ggtitle("All Species 2009 - 2015")+ 
+  labs(x="Cumulative Area (km2)",y="Cumulative Larvae (%)") +
+  theme(plot.title = element_text(face="bold", size=28)) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(axis.title = element_text(face="bold", size=26)) +
+  theme(axis.text=element_text(face="bold", size=20, colour = "black"))+
+  geom_point()  +
+  theme( panel.grid.minor.x = element_blank() ,
+    panel.grid.minor.y = element_blank()) +
+  geom_segment(x=0, xend=d_20$cum_area_km2, y=d_20$cum_pct_quantity, yend=d_20$cum_pct_quantity, size = 1.5, color = "red") +
+  geom_segment(x=d_20$cum_area_km2, xend=d_20$cum_area_km2, y=0, yend=d_20$cum_pct_quantity,size = 1.5, color = "red") +
+  geom_segment(x=0, xend=d_40$cum_area_km2, y=d_40$cum_pct_quantity, yend=d_40$cum_pct_quantity,size = 1.5, color = "orange") +
+  geom_segment(x=d_40$cum_area_km2, xend=d_40$cum_area_km2, y=0, yend=d_40$cum_pct_quantity,size = 1.5, color = "orange") +
+  # geom_segment(x=0, xend=d_60$cum_area_km2, y=d_60$cum_pct_quantity, yend=d_60$cum_pct_quantity,size = 1.5, color = "yellow") +
+  # geom_segment(x=d_60$cum_area_km2, xend=d_60$cum_area_km2, y=0, yend=d_60$cum_pct_quantity,size = 1.5, color = "yellow") +
+  scale_y_continuous(expand = c(0,0), breaks = c(20,40,60,80,100)) + 
+  scale_x_continuous(expand = c(0,0)) +
+  #scale_x_continuous() +
+  coord_cartesian(xlim = c(0, max_area_km2)) 
   # coord_cartesian(xlim = c(0, tail(d$cum_area_km2, 1)), ylim = c(0, 100))
 print(p)
+
+windows()
+p
+
+
 ggplot2::ggsave('all_sp_area_graph.png', p)
+
 ggplotly(p)
-
-
 
 
 plot(r)
@@ -282,34 +378,10 @@ plot(r)
 
 
 # todo ----
-
-# for (dir in c('sp_2009','sp_2010','sp_2011','sp_2012', 'sp_2013', 'sp_2014', 'sp_2015')){
-#   summarize_sppyr('G:/Team_Folders/Steph/sp_2009')
-# }
-
-
 # - create github.com/graysreef organization
 # - create R package inside github.com/graysreef/mget-conn-process repository
 #     using http://ucsb-bren.github.io/env-info/wk07_package.html
 # - create Dan's plot: x) cumulative percent larvel input vs y) area of included ranked patches
-
-#aggregate csvs ---- 
-path <- 'G:/Team_Folders/Steph/rs_2015'
-setwd(path)
-
-my.dirs <- dir(pattern = "results", include.dirs = T)
-
-
-for (i in 1:length(my.dirs)){
-  file <- paste0("./",my.dirs[i], "/connectivity.csv")
-  print(file)
-  my.csv <- read.csv(file)
-
-}
-
-
-
-
 
 #Process for percent instead of quanity----
 # 
@@ -339,7 +411,7 @@ library(tidyverse)
 library(raster)
 library(plotly)
 
-r = raster('G:/Team_Folders/Steph/bsb/mean.tif')
+r = raster('G:/Team_Folders/Steph/_allspp/mean_sum.tif')
 
 d = data_frame(
   quantity = raster::getValues(r),
@@ -360,17 +432,13 @@ d3 = d %>%
   left_join(d2, by='cellid')
 summary(d3)
 
-r2 = setValues(r, d3$pct_quantity)
+r2 = setValues(r, d3$cum_pct_quantity) #cum_pct_quantity or pct_quantity based on if you want contour or not
 
 plot(r2) 
 
-writeRaster(r2, "percent.tif", format = "GTiff")
+writeRaster(r2, "cum_percent_sum.tif", format = "GTiff")
 
-
-
-
-
-##sensitivities
+#sensitivities ---- 
 process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2009_diffusivity')
 process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2009_mortality')
 process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2009_competency')
@@ -379,14 +447,14 @@ summarize_sppyr('G:/Team_Folders/Steph/bsb_2009_all')
 process_sppyr_dirs('G:/Team_Folders/Steph/rs_2009_competency')
 process_sppyr_dirs('G:/Team_Folders/Steph/rs_2009_diffusivity')
 
-
 # done ----
 # process_geodb(
 #   'G:/Team_Folders/Steph/bsb_2015/5_4_15_FM_bsb_50day_results',
 #   'G:/Team_Folders/Steph/bsb_2015/5_4_15_FM_bsb_50day_simulation')
 #process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2015', do_csv=F, do_tif=F, do_png=T)
 #summarize_sppyr('G:/Team_Folders/Steph/bsb_2015')
-# processed speices per Individual year---- 
+ 
+# processed speices per Individual year
 # process_sppyr_dirs('G:/Team_Folders/Steph/gg_2009')
 # process_sppyr_dirs('G:/Team_Folders/Steph/gg_2010')
 # process_sppyr_dirs('G:/Team_Folders/Steph/gg_2011')
@@ -456,6 +524,70 @@ process_sppyr_dirs('G:/Team_Folders/Steph/rs_2009_diffusivity')
 # summarize_sppyr('G:/Team_Folders/Steph/bsb_2014')
 # summarize_sppyr('G:/Team_Folders/Steph/bsb_2015')
 # summarize_sppyr('G:/Team_Folders/Steph/bsb_2015_all')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2009')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2010')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2011')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2012')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2013')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2014')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2015')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2009')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2010')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2011')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2012')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2013')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2014')
+# summarize_sppyr('G:/Team_Folders/Steph/rs_2015')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2009')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2010')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2011')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2012')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2013')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2014')
+# summarize_sppyr('G:/Team_Folders/Steph/gg_2015')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2009')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2010')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2011')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2012')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2013')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2014')
+# summarize_sppyr('G:/Team_Folders/Steph/sp_2015')
+# 
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2009_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2010_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2011_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2012_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2013_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2014_expanded')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2015_expanded')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2009_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2010_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2011_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2012_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2013_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2014_expanded')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2015_expanded')
+# 
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2009_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2010_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2011_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2012_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2013_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2014_from_gr')
+# process_sppyr_dirs('G:/Team_Folders/Steph/bsb_2015_from_gr')
+# 
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2009_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2010_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2011_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2012_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2013_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2014_from_gr')
+# summarize_sppyr('G:/Team_Folders/Steph/bsb_2015_from_gr')
 
 #Processing for mortality because it has a differently named geodatabase----
 process_singledir = function(dir_results, dir_simulation, do_csv=T, do_tif=T, do_png=T){
